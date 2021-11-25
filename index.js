3ac20eb1a54e65c277c6192d0b8a2b4a511733ef
“Jing <+>
2021-11-24 22:30:33 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield",
+];
+
+function destructivelyAppendCat (name) {
+    cats.push ("Ralph");
+};
+
+
+function destructivelyPrependCat (name) {
+    cats.unshift ("Bob");
+};
+
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop()
+};
+
+function destructivelyRemoveFirstCat (name) {
+    cats.shift()
+};
+
+function appendCat(name) {
+    const copyOfCat = [...cats];
+    copyOfCat.push ("Broom");
+    return copyOfCat;
+};
+
+function prependCat(name) {
+    const newCopyOfCat = [...cats];
+    newCopyOfCat.unshift ("Arnold");
+    return newCopyOfCat;
+};
+
+function removeLastCat(name) {
+    const aNewCopyOfCat = [...cats];
+    aNewCopyOfCat.pop ();
+    return aNewCopyOfCat;
+};
+
+function removeFirstCat(name) {
+    const anotherNewCopyOfCat = [...cats];
+    anotherNewCopyOfCat.shift ();
+    return anotherNewCopyOfCat;
+};
